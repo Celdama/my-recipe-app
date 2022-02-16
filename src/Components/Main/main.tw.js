2734286796d0c6a1256770157a8a0c8b9a209be2
Celdama <celdama.dev@gmail.com>
2022-02-16 14:13:29 +0100

    add some style to RecipeCard component

--- src/Components/Main/main.tw.js
+++ src/Components/Main/main.tw.js
@@ -15,9 +15,4 @@ export const Content = tw.div`
 `;
 
 export const Container = tw.div`
-  border-4 
-  border-dashed 
-  border-gray-200 
-  rounded-lg 
-  h-96
 `;
--- src/Components/Recipes/index.js
+++ src/Components/Recipes/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { recipesSelector } from '../../store/selectors/recipesSelector';
+import RecipeCard from '../RecipeCard';
 
 export const Recipes = ({ recipes }) => {
-  console.log(recipes);
-  return <div>index</div>;
+  return (
+    <div className='flex flex-wrap justify-center'>
+      {recipes.map((recipe, index) => (
+        <RecipeCard recipe={recipe} key={index} />
+      ))}
+    </div>
+  );
 };
 
 export const RecipesStore = () => {
