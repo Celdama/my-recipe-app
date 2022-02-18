dff89e06b90ee5c42bcf2af57e80ffe2d61cf70c
Celdama <celdama.dev@gmail.com>
2022-02-18 10:39:32 +0100

    Components/404/index: update content

--- src/Components/404/index.js
+++ src/Components/404/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const NoMatch = () => {
-  let location = useLocation();
-
   return (
-    <div>
-      <p className='mb-4'>
-        Sorry nothing to see on <code>{location.pathname}</code>
-      </p>
+    <div className='h-full flex flex-col justify-center items-center mt-20'>
+      <h1 className='mb-4 text-5xl font-semibold text-center'>
+        Uh oh. That page doesn't exit.
+      </h1>
       <Link
-        className='text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800 dark:border-gray-700'
+        className='text-white mt-12 bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800 dark:border-gray-700'
         to='/'
       >
         Back to Home Page
