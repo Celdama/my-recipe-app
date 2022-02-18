ddb9afbd749513633663a015891b042ad505f579
Celdama <celdama.dev@gmail.com>
2022-02-18 11:50:28 +0100

    Components/404/index: end style and content of 404 component

--- src/Components/404/index.js
+++ src/Components/404/index.js
@@ -1,17 +1,88 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  EyeIcon,
+  HeartIcon,
+  PlusIcon,
+  ChevronRightIcon,
+  ArrowRightIcon,
+} from '@heroicons/react/outline';
 
 const NoMatch = () => {
-  return (
-    <div className='h-full flex flex-col justify-center items-center mt-20'>
-      <h1 className='mb-4 text-5xl font-semibold text-center'>
-        Uh oh. That page doesn't exit.
-      </h1>
+  const popularPages = [
+    {
+      title: 'discover',
+      href: 'discover',
+      desc: 'Find every day 4 recipes drawn',
+    },
+    {
+      title: 'favourite',
+      href: 'favourite',
+      desc: 'Find your favorite recipes',
+    },
+    {
+      title: 'add recipe',
+      href: 'add-recipe',
+      desc: 'The page to allow you to add new recipes',
+    },
+  ];
+
+  const popularContent = popularPages.map((page) => {
+    let icon;
+
+    if (page.title === 'discover') {
+      icon = <EyeIcon className='h-5 w-5 text-indigo-500' />;
+    } else if (page.title === 'favourite') {
+      icon = <HeartIcon className='h-5 w-5 text-indigo-500' />;
+    } else {
+      icon = <PlusIcon className='h-5 w-5 text-indigo-500' />;
+    }
+
+    return (
       <Link
-        className='text-white mt-12 bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800 dark:border-gray-700'
-        to='/'
+        to={page.href}
+        className={
+          page.title === 'discover'
+            ? ' flex items-center justify-between py-4 hover:cursor-pointer   border-t'
+            : 'flex items-center justify-between py-4 hover:cursor-pointer'
+        }
       >
-        Back to Home Page
+        <div className='flex gap-3'>
+          <div className='bg-gray-200 rounded-md p-3'>{icon}</div>
+          <div>
+            <h5 className=' font-semibold capitalize leading-tight'>
+              {page.title}
+            </h5>
+            <p className='text-slate-500'>{page.desc}</p>
+          </div>
+        </div>
+        <ChevronRightIcon className='h-5 w-5 text-slate-600' />
+      </Link>
+    );
+  });
+
+  return (
+    <div className='mx-auto mt-16 max-w-screen-sm font-sans'>
+      <div className='flex flex-col justify-center items-center'>
+        <span className='text-center block font-bold text-indigo-500'>
+          404 ERROR
+        </span>
+        <h1 className='mb-4 text-5xl md:text-6xl font-bold text-center'>
+          This page does not exist.
+        </h1>
+        <p className='text-lg text-slate-600'>
+          The page you are looking for could not be found.
+        </p>
+      </div>
+      <div className='divide-y divide-y-reverse divide-slate-200 pb-9 mt-9'>
+        <h2 className='mb-3 uppercase font-semibold text-slate-600'>
+          Popular pages
+        </h2>
+        <div className='divide-y divide-slate-200'>{popularContent}</div>
+      </div>
+      <Link className='flex items-center gap-1 py-4 h-8' to='/'>
+        <span className='text-indigo-500 font-semibold'>Or go back home </span>
+        <ArrowRightIcon className='h-5 w-4 pt-1 text-indigo-500' />
       </Link>
     </div>
   );
