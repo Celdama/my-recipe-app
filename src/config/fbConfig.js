e9581df9117af6d5004648fc1779d0b6ec5c5ab7
Celdama <celdama.dev@gmail.com>
2022-02-22 10:00:32 +0100

    install firebase and create config files

---
+++ src/config/fbConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB_QwzzWUehIJRZ0EFucj3I9vJtgIczr-Q',
+  authDomain: 'my-recipe-app-49544.firebaseapp.com',
+  projectId: 'my-recipe-app-49544',
+  storageBucket: 'my-recipe-app-49544.appspot.com',
+  messagingSenderId: '170833709655',
+  appId: '1:170833709655:web:5a1aac2e5008861f2db112',
+  measurementId: 'G-2THLSBWX5B',
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
