ea0f25a2768c3ef0f0f75e0aa301d3d23223e7d9
Celdama <celdama.dev@gmail.com>
2022-02-24 10:48:35 +0100

    store: create action to get users list and selector

--- src/store/actions/usersAction.js
+++ src/store/actions/usersAction.js
@@ -1,8 +1,23 @@
-import { ADD_USER } from '../reducers/usersReducer';
+import { ADD_USER, GET_USERS } from '../reducers/usersReducer';
 import { collection, getDocs, addDoc } from 'firebase/firestore';
 import { db } from '../../config/fbConfig';
 
 const usersCollectionRef = collection(db, 'users');
+
+export const getUsers = () => {
+  return async (dispatch) => {
+    try {
+      const data = await getDocs(usersCollectionRef);
+      const res = data.docs.map((doc) => ({ ...doc.data() }));
+      dispatch({
+        type: GET_USERS,
+        payload: res,
+      });
+    } catch (err) {
+      return console.log(err);
+    }
+  };
+};
 
 export const addUser = (data) => {
   return async (dispatch) => {
--- src/store/reducers/usersReducer.js
+++ src/store/reducers/usersReducer.js
@@ -1,4 +1,5 @@
 export const ADD_USER = 'ADD_USER';
+export const GET_USERS = 'GET_USERS';
 
 const initialState = [];
 
@@ -6,6 +7,8 @@ export const usersReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_USER:
       return [];
+    case GET_USERS:
+      return [...state, ...action.payload];
     default:
       return state;
   }
