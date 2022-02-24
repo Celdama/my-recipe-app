b6cba8f8461023d2c41b3205e63a1cef672f1814
Celdama <celdama.dev@gmail.com>
2022-02-24 17:24:39 +0100

    fix issus chefs is undefined, so i use useEffect to get users on load ChefDetailsStore component

--- src/Components/ChefDetails/index.js
+++ src/Components/ChefDetails/index.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { usersSelector } from '../../store/selectors/usersSelector';
 import { recipesSelector } from '../../store/selectors/recipesSelector';
 import RecipeCard from '../RecipeCard';
+import { useDispatch } from 'react-redux';
+import { getUsers } from '../../store/actions/usersAction';
 
 export const ChefDetails = ({ chef, chefRecipes }) => {
   return (
     <div>
-      <h1>{chef.userName}</h1>
-      <h4>{chef.email}</h4>
-      <p>Number of recipes : {chefRecipes.length}</p>
-      <div className='flex flex-wrap justify-center'>
-        {chefRecipes.map((recipe) => (
-          <RecipeCard key={recipe.id} recipe={recipe} />
-        ))}
-      </div>
+      {chef && chefRecipes && (
+        <>
+          <h1>{chef.userName}</h1>
+          <h4>{chef.email}</h4>
+          <p>Number of recipes : {chefRecipes.length}</p>
+          <div className='flex flex-wrap justify-center'>
+            {chefRecipes.map((recipe) => (
+              <RecipeCard key={recipe.id} recipe={recipe} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
 
 export const ChefDetailsStore = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+
   const { id } = useParams();
   const chefs = useSelector(usersSelector);
   const recipes = useSelector(recipesSelector);
