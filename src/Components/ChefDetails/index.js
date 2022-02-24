d74d02c061d115be356a2de57ec2bdad2292a3b5
Celdama <celdama.dev@gmail.com>
2022-02-24 16:55:20 +0100

    Components/ChefDetails/index: display each recipe chef in chefDetails component

--- src/Components/ChefDetails/index.js
+++ src/Components/ChefDetails/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { usersSelector } from '../../store/selectors/usersSelector';
+import { recipesSelector } from '../../store/selectors/recipesSelector';
+import RecipeCard from '../RecipeCard';
 
-export const ChefDetails = () => {
-  return <div>chef details</div>;
+export const ChefDetails = ({ chef, chefRecipes }) => {
+  return (
+    <div>
+      <h1>{chef.userName}</h1>
+      <h4>{chef.email}</h4>
+      <p>Number of recipes : {chefRecipes.length}</p>
+      <div className='flex flex-wrap justify-center'>
+        {chefRecipes.map((recipe) => (
+          <RecipeCard key={recipe.id} recipe={recipe} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export const ChefDetailsStore = () => {
   const { id } = useParams();
+  const chefs = useSelector(usersSelector);
+  const recipes = useSelector(recipesSelector);
+  const chef = chefs.filter((chef) => chef.uid === id)[0];
+  const chefRecipes = recipes.filter(
+    (recipe) => recipe.authorEmail === chef.email
+  );
 
-  console.log(id);
-
-  return <ChefDetails />;
+  return <ChefDetails chef={chef} chefRecipes={chefRecipes} />;
 };
