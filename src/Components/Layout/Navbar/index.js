7f89866f411a09977d06ace293b6d1224be4004f
Celdama <celdama.dev@gmail.com>
2022-03-01 06:56:08 +0100

    render add Recipe in nav mobile links

--- src/Components/Layout/Navbar/index.js
+++ src/Components/Layout/Navbar/index.js
@@ -158,6 +158,23 @@ export const Navbar = ({ handleSignOut, isLogin, authUser }) => {
                     {name}
                   </Disclosure.Button>
                 ))}
+                {isLogin &&
+                  loginNavigation.map(({ name, href, current }) => (
+                    <Disclosure.Button
+                      as={NavLink}
+                      key={name}
+                      to={href}
+                      className={classNames(
+                        current
+                          ? 'bg-gray-800 text-white'
+                          : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'block px-3 py-2 rounded-md text-base font-medium'
+                      )}
+                      aria-current={current ? 'page' : undefined}
+                    >
+                      {name}
+                    </Disclosure.Button>
+                  ))}
               </NavItemsMobileContainer>
 
               <NavUserMobileContainer>
