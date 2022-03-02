4a902454baa3d05b2d33b18f251b17c7e0c9797e
Celdama <celdama.dev@gmail.com>
2022-03-02 09:25:02 +0100

    Components/Recipe/MyRecipes/index: add CallToAction component

--- src/Components/Recipe/MyRecipes/index.js
+++ src/Components/Recipe/MyRecipes/index.js
@@ -4,11 +4,15 @@ import { authSelector } from '../../../store/selectors/authSelector';
 import { recipesSelector } from '../../../store/selectors/recipesSelector';
 import RecipeCard from '../RecipeCard';
 import Spinner from '../../Layout/Spinner';
+import CallToAction from '../../CallToAction';
 
 export const MyRecipes = ({ recipes, authUser }) => {
+  const topText = 'You need to be connected to see your recipes list';
+  const bottomText = 'Create an account and add your first recipe.';
+
   const myRecipesContent = (recipesList, userId) => {
     if (authUser.uid === undefined) {
-      return <p>you need to be connected to see your recipes list</p>;
+      return <CallToAction topText={topText} bottomText={bottomText} />;
     }
 
     if (!recipesList.some((recipe) => recipe.authorId === userId)) {
@@ -29,12 +33,12 @@ export const MyRecipes = ({ recipes, authUser }) => {
       {!recipes.length ? (
         <Spinner />
       ) : (
-        <>
+        <div>
           {!!authUser.uid && recipesContent.length && (
             <h1>This is your delicious recipes list</h1>
           )}
           {recipesContent}
-        </>
+        </div>
       )}
     </div>
   );
