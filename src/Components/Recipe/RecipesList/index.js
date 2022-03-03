9348fed2d77d6e582ac7d78afa40cfb70f82fb38
Celdama <celdama.dev@gmail.com>
2022-03-03 08:57:00 +0100

    Components/Recipe/RecipesList/index: added proptypes

--- src/Components/Recipe/RecipesList/index.js
+++ src/Components/Recipe/RecipesList/index.js
@@ -6,6 +6,7 @@ import { useDispatch } from 'react-redux';
 import { resetCurrentRecipe } from '../../../store/actions/currentRecipeAction';
 import { Wrapper } from './recipesList.tw';
 import Spinner from '../../Layout/Spinner';
+import PropTypes from 'prop-types';
 
 export const RecipesList = ({ recipes }) => {
   const recipesContent =
@@ -13,6 +14,10 @@ export const RecipesList = ({ recipes }) => {
     recipes.map((recipe, index) => <RecipeCard key={index} recipe={recipe} />);
 
   return <Wrapper>{!recipes.length ? <Spinner /> : recipesContent}</Wrapper>;
+};
+
+RecipesList.propTypes = {
+  recipes: PropTypes.array,
 };
 
 export const RecipesListStore = () => {
