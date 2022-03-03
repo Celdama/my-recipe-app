5a30410835997dfaa2ea330073aff2b824883216
Celdama <celdama.dev@gmail.com>
2022-03-03 08:53:38 +0100

    Components/Recipe/RecipeIngredients/index: added proptypes

--- src/Components/Recipe/RecipeIngredients/index.js
+++ src/Components/Recipe/RecipeIngredients/index.js
@@ -4,18 +4,23 @@ import {
   IngredientsList,
   IngredientItem,
 } from './recipeIngredients.tw';
+import PropTypes from 'prop-types';
 
 const RecipeIngredients = ({ ingredients }) => {
+  const ingredientsContent = ingredients.map((ingredient, i) => {
+    return <IngredientItem key={i}>{ingredient}</IngredientItem>;
+  });
+
   return (
     <Wrapper>
       <h3 className='title-section-recipe-detail'>Ingredients</h3>
-      <IngredientsList>
-        {ingredients.map((ingredient, i) => {
-          return <IngredientItem key={i}>{ingredient}</IngredientItem>;
-        })}
-      </IngredientsList>
+      <IngredientsList>{ingredientsContent}</IngredientsList>
     </Wrapper>
   );
+};
+
+RecipeIngredients.propTypes = {
+  ingredients: PropTypes.array,
 };
 
 export default RecipeIngredients;
