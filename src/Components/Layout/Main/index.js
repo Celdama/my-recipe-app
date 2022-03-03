0fa85912e5611e697b2a18855fa6e8dcdf25e880
Celdama <celdama.dev@gmail.com>
2022-03-03 08:23:58 +0100

    Components/Layout/Main/index: added proptypes

--- src/Components/Layout/Main/index.js
+++ src/Components/Layout/Main/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Wrapper, Content, Container } from './main.tw';
+import PropTypes from 'prop-types';
 
 const Main = ({ children }) => {
   return (
@@ -11,6 +12,10 @@ const Main = ({ children }) => {
       </Wrapper>
     </main>
   );
+};
+
+Main.propTypes = {
+  children: PropTypes.object,
 };
 
 export default Main;
