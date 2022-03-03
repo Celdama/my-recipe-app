57dc54ad04fdff7e405bf4b03bafff6e772c6a7b
Celdama <celdama.dev@gmail.com>
2022-03-03 09:20:38 +0100

    Components/Chef/ChefsList/index: fix little issu with get user on cheflist load

--- src/Components/Chef/ChefsList/index.js
+++ src/Components/Chef/ChefsList/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { getUsers } from '../../../store/actions/usersAction';
 import { usersSelector } from '../../../store/selectors/usersSelector';
 import ChefCard from '../ChefCard';
 import Spinner from '../../Layout/Spinner';
@@ -8,6 +10,7 @@ import { Header, Wrapper, Title, SubTitle, Content } from './chefsList.tw';
 import PropTypes from 'prop-types';
 
 export const ChefsList = ({ chefs }) => {
+  console.log(chefs);
   const chefsContent =
     chefs && chefs.map((chef) => <ChefCard key={chef.uid} chef={chef} />);
 
@@ -39,7 +42,12 @@ ChefsList.propTypes = {
 };
 
 export const ChefsListStore = () => {
+  const dispatch = useDispatch();
   const chefs = useSelector(usersSelector);
+
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
 
   return <ChefsList chefs={chefs} />;
 };
