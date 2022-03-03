cfa42bb634d827fd11d12e19d27dc6d5a7b27a71
Celdama <celdama.dev@gmail.com>
2022-03-03 08:27:30 +0100

    Components/Profile/index: added proptypes

--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { authSelector } from '../../store/selectors/authSelector';
+import PropTypes from 'prop-types';
 
 export const Profile = ({ authUser }) => {
   const { displayName, photoURL } = authUser;
@@ -11,6 +12,10 @@ export const Profile = ({ authUser }) => {
       <img src={photoURL} className='h-24 w-24' alt='user avatar' />
     </div>
   );
+};
+
+Profile.propTypes = {
+  authUser: PropTypes.object,
 };
 
 export const ProfileStore = () => {
